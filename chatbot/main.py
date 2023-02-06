a3c0e946e83c90024ccf25215181f50b85c0efeb
aidenlu <aiden_lu@wochacha.com>
2023-02-06 18:51:58 +0800

    fix: call dingtalk webhook

--- chatbot/main.py
+++ chatbot/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Body
 from .gpt import AsyncChatbot
 
 from .models import DingtalkAskMessage
+import httpx
 
 app = FastAPI()
 
@@ -18,6 +19,7 @@ async def chat(message: DingtalkAskMessage):
     prompt = message.text.content.strip()
     nickname = message.senderNick
     sender_userid = message.senderStaffId
+    webhook_url = message.sessionWebhook
     if prompt.startswith("清空会话"):
         chatbot.reset()
 
@@ -26,5 +28,9 @@ async def chat(message: DingtalkAskMessage):
     payload = {"text": {"content": reply}, "msgtype": "text"}
     if sender_userid:
         payload["at"] = {"atUserIds": [sender_userid]}
+
+    async with httpx.AsyncClient() as client:
+        r = await client.post(webhook_url, json=payload)
+        assert r.status_code == 200, "钉钉回调失败!"
 
     return payload

a7dbadea7e455b4b1f28aba6ecf56500b8999dcd
aidenlu <aiden_lu@wochacha.com>
2023-02-06 18:23:19 +0800

    fix: make `@` effect

--- chatbot/main.py
+++ chatbot/main.py
@@ -17,9 +17,14 @@ chatbot = AsyncChatbot()
 async def chat(message: DingtalkAskMessage):
     prompt = message.text.content.strip()
     nickname = message.senderNick
+    sender_userid = message.senderStaffId
     if prompt.startswith("清空会话"):
         chatbot.reset()
 
     response = await chatbot.ask(prompt, temperature=temperature, user=nickname)
     reply = response["choices"][0]["text"].strip()
-    return {"text": {"content": reply}, "msgtype": "text"}
+    payload = {"text": {"content": reply}, "msgtype": "text"}
+    if sender_userid:
+        payload["at"] = {"atUserIds": [sender_userid]}
+
+    return payload

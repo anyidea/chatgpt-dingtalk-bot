806c0e96a72dac48782c526d406b77b78aa611cf
aiden <allaher@icloud.com>
2023-06-17 13:15:15 +0800

    Update templates.py

--- chatbot/templates.py
+++ chatbot/templates.py
@@ -1,9 +1,9 @@
 INTERACTIVE_CARD_JSON_SAMPLE = {
     "config": {"autoLayout": True, "enableForward": True},
-    "header": {
-        "title": {"type": "text", "text": "AI助手"},
-        "logo": "@lALPDfJ6V_FPDmvNAfTNAfQ",
-    },
+#     "header": {
+#         "title": {"type": "text", "text": "AI助手"},
+#         "logo": "@lALPDfJ6V_FPDmvNAfTNAfQ",
+#     },
     "contents": [
         {
             "type": "text",

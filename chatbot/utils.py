7ae5dce696020081776e3d76861cd4c0d31af24d
aidenlu <aiden_lu@wochacha.com>
2023-12-22 10:28:07 +0800

    fix: fix bugs

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -1,27 +1,18 @@
 import uuid
 import os
-from revChatGPT import V3
+from revChatGPT.V3 import ENGINES, Chatbot
 
 from .config import gpt_settings
 
-V3.ENGINES = [
-    "gpt-3.5-turbo",
-    "gpt-3.5-turbo-16k",
-    "gpt-3.5-turbo-0301",
-    "gpt-3.5-turbo-0613",
-    "gpt-3.5-turbo-16k-0613",
-    "gpt-3.5-turbo-1106",
-    "gpt-3.5-turbo-instruct",
-    "gpt-3.5-turbo-instruct-0914",
-    "gpt-4",
-    "gpt-4-0314",
-    "gpt-4-32k",
-    "gpt-4-32k-0314",
-    "gpt-4-0613",
-    "gpt-4-32k-0613",
-    "gpt-4-1106-preview",
-    "gpt-4-vision-preview",
-]
+ENGINES.extend(
+    [
+        "gpt-3.5-turbo-1106",
+        "gpt-3.5-turbo-instruct",
+        "gpt-3.5-turbo-instruct-0914",
+        "gpt-4-1106-preview",
+        "gpt-4-vision-preview",
+    ]
+)
 
 
 def get_conversation_id(convo_id: str):
@@ -54,7 +45,7 @@ def init_chatbot():
         "truncate_limit": config.truncate_limit,
     }
     kwargs = {k: v for k, v in kwargs.items() if v is not None}
-    bot = V3.Chatbot(**kwargs)
+    bot = Chatbot(**kwargs)
     return bot
 
 

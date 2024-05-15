934bd29e066b35e659b79235ff1263a044ae3344
aidenlu <aiden_lu@wochacha.com>
2024-05-15 10:36:04 +0800

    feat: add more models support

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -9,8 +9,12 @@ ENGINES.extend(
         "gpt-3.5-turbo-1106",
         "gpt-3.5-turbo-instruct",
         "gpt-3.5-turbo-instruct-0914",
+        "gpt-4-32k",
         "gpt-4-1106-preview",
         "gpt-4-vision-preview",
+        "gpt-4-turbo-preview",
+        "gpt-4-turbo",
+        "gpt-4-turbo-2024-04-09",
         "gpt-4o",
         "gpt-4o-2024-05-13",
     ]
